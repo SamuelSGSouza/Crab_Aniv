9ef89a20b2de9350f8f04f792f261188f1e7c9fb
Samuel Santos Gomes de Souza <Samuels.g.desouza@gmail.com>
2024-07-29 23:36:34 -0300

    att

--- jogos_magicos/urls.py
+++ jogos_magicos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('adm_index/', views.AdmIndexView.as_view(), name='adm_index'),
 ]
--- jogos_magicos/views.py
+++ jogos_magicos/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class AdmIndexView(TemplateView):
+    template_name = 'adm_index.html'
